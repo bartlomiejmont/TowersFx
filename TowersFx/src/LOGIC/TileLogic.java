0e1fca510d028d77b796c71f6453fe1674ec5400
Bartomiej Mont <spankertroll77@gmail.com>
2019-12-28 18:23:46 +0100

    checking if tile was already visited

--- TowersFx/src/LOGIC/TileLogic.java
+++ TowersFx/src/LOGIC/TileLogic.java
@@ -42,19 +42,19 @@ public class TileLogic {
         Stack<Tile> possibleMoves = new Stack<Tile>();
         System.out.println(MainLogic.lastTile.tileInfo());
             // top
-        if(MainLogic.lastTile.getTableX()>=0 && MainLogic.lastTile.getTableX()<=MainLogic.width-1 && MainLogic.lastTile.getTableY()+1>=0 && MainLogic.lastTile.getTableY()+1<=MainLogic.height-1){
+        if(MainLogic.lastTile.getTableX()>=0 && MainLogic.lastTile.getTableX()<=MainLogic.width-1 && MainLogic.lastTile.getTableY()+1>=0 && MainLogic.lastTile.getTableY()+1<=MainLogic.height-1 && !MainLogic.moves.contains(MainLogic.tileMap[MainLogic.lastTile.getTableX()][MainLogic.lastTile.getTableY()+1])){
             possibleMoves.push(MainLogic.tileMap[MainLogic.lastTile.getTableX()][MainLogic.lastTile.getTableY()+1]);
         }
             // bottom
-        if(MainLogic.lastTile.getTableX()>=0 && MainLogic.lastTile.getTableX()<=MainLogic.width-1 && MainLogic.lastTile.getTableY()-1>=0 && MainLogic.lastTile.getTableY()-1<=MainLogic.height-1){
+        if(MainLogic.lastTile.getTableX()>=0 && MainLogic.lastTile.getTableX()<=MainLogic.width-1 && MainLogic.lastTile.getTableY()-1>=0 && MainLogic.lastTile.getTableY()-1<=MainLogic.height-1 && !MainLogic.moves.contains(MainLogic.tileMap[MainLogic.lastTile.getTableX()][MainLogic.lastTile.getTableY()-1])){
             possibleMoves.push(MainLogic.tileMap[MainLogic.lastTile.getTableX()][MainLogic.lastTile.getTableY()-1]);
         }
             // right
-        if(MainLogic.lastTile.getTableX()+1>=0 && MainLogic.lastTile.getTableX()+1<=MainLogic.width-1 && MainLogic.lastTile.getTableY()>=0 && MainLogic.lastTile.getTableY()<=MainLogic.height-1){
+        if(MainLogic.lastTile.getTableX()+1>=0 && MainLogic.lastTile.getTableX()+1<=MainLogic.width-1 && MainLogic.lastTile.getTableY()>=0 && MainLogic.lastTile.getTableY()<=MainLogic.height-1 && !MainLogic.moves.contains(MainLogic.tileMap[MainLogic.lastTile.getTableX()+1][MainLogic.lastTile.getTableY()])){
             possibleMoves.push(MainLogic.tileMap[MainLogic.lastTile.getTableX()+1][MainLogic.lastTile.getTableY()]);
         }
             // left
-        if(MainLogic.lastTile.getTableX()-1>=0 && MainLogic.lastTile.getTableX()-1<=MainLogic.width-1 && MainLogic.lastTile.getTableY()>=0 && MainLogic.lastTile.getTableY()<=MainLogic.height-1){
+        if(MainLogic.lastTile.getTableX()-1>=0 && MainLogic.lastTile.getTableX()-1<=MainLogic.width-1 && MainLogic.lastTile.getTableY()>=0 && MainLogic.lastTile.getTableY()<=MainLogic.height-1 && !MainLogic.moves.contains(MainLogic.tileMap[MainLogic.lastTile.getTableX()-1][MainLogic.lastTile.getTableY()])){
             possibleMoves.push(MainLogic.tileMap[MainLogic.lastTile.getTableX()-1][MainLogic.lastTile.getTableY()]);
         }
 
